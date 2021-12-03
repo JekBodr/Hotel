4e136123e208de60c5363e64d38f563585e21b40
JekBodr <jekbodr@gmail.com>
2021-12-03 02:03:17 +0200

    Start version: StarsChose

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import controllers.ClientController;
+import models.Client;
+import views.ClientView;
+
 public class Main {
     public static void main(String[] args) {
 
+            System.out.println("Модификация проекта, добавлен рассчет налога");
+
+            Client model = new Client();
+            ClientView view = new ClientView(model);
+            ClientController controller = new ClientController(model, view);
+            // Запуск программы/приложения
+            controller.runApp();
     }
 }
---
+++ src/models/Client.java
@@ -0,0 +1,57 @@
+package models;
+
+import base.StarsChose;
+import utils.Validator;
+
+import java.util.Scanner;
+
+public class Client implements StarsChose {
+    private String name;
+    private int nightQuantity;
+    private int guestsQuantity;
+    private int starsQuantity;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNightQuantity() {
+        return nightQuantity;
+    }
+
+    public void setNightQuantity(int nightQuantity) {
+        this.nightQuantity = nightQuantity;
+    }
+
+    public int getGuestsQuantity() {
+        return guestsQuantity;
+    }
+
+    public void setGuestsQuantity(int guestsQuantity) {
+        this.guestsQuantity = guestsQuantity;
+    }
+
+    public int getStarsQuantity() {
+        return starsQuantity;
+    }
+
+    public void setStarsQuantity(int starsQuantity) {
+        this.starsQuantity = starsQuantity;
+    }
+
+    // Расчёт стоимости номера в зависимости от коллчества звезд отеля.
+    @Override
+    public double calculateStarsPrice(int stars) {
+       double sum = 0;
+       if (stars == 3) sum = 500;
+       else if (stars == 4) sum = 750;
+       else if (stars == 5) sum = 1000;
+       return sum;
+
+    }
+}
+
