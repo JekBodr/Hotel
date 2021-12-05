4624593d789b31b39ec2977745a7b2a05c7ebc1b
JekBodr <jekbodr@gmail.com>
2021-12-05 23:10:48 +0200

    Add Main

--- src/controllers/ReservationController.java
+++ src/controllers/ReservationController.java
@@ -1,8 +1,29 @@
 package controllers;
 
+import models.Reservation;
+import views.ReservationView;
 
 public class ReservationController {
 
+    ReservationView view;
+    Reservation model;
+
+
+    public ReservationController(ReservationView view, Reservation model) {
+        this.view = view;
+        this.model = model;
+    }
+
+    public void runApp(){
+        view.getInputs();
+
+
+
+        //view.getOutput();
+
+
+
+    }
 
 
 }
--- src/views/ReservationView.java
+++ src/views/ReservationView.java
@@ -8,48 +8,56 @@ import java.util.Date;
 import java.util.Scanner;
 
 public class ReservationView {
-
-
-
+    Reservation model;
     Scanner sc = new Scanner(System.in);
     SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
-    try {
-        System.out.print("Номер комнаты: ");
-        int roomNumber = sc.nextInt();
-        System.out.print("Дата заезда (dd/MM/yyyy): ");
-        Date checkIn = null;
-        try {
-            checkIn = sdf.parse(sc.next());
-        } catch (ParseException ex) {
-            ex.printStackTrace();
-        }
-        System.out.print("Дата выезда (dd/MM/yyyy): ");
-        Date checkOut = null;
-        try {
-            checkOut = sdf.parse(sc.next());
-        } catch (ParseException ex) {
-            ex.printStackTrace();
-        }
-
-        Reservation reservation = new Reservation(roomNumber, checkIn, checkOut);
-        System.out.println("Бронирование: " + reservation);
-
-        System.out.println("\nВведите данные для обновления бронирования:");
-        System.out.print("Дата заезда (dd/MM/yyyy): ");
-        checkIn = sdf.parse(sc.next());
-        System.out.print("Дата выезда (dd/MM/yyyy): ");
-        checkOut = sdf.parse(sc.next());
-
-        reservation.updateDates(checkIn, checkOut);
-        System.out.println("Бронирование: " + reservation);
-    } catch (ParseException e) {
-        System.out.println("Неверный формат даты");
-    } catch (DomainException e) {
-        System.out.println("Ошибка при бронировании: " + e.getMessage());
-    } catch (RuntimeException e) {
-        System.out.println("Неожиданная ошибка");
+    public ReservationView(Reservation model) throws ParseException {
+        this.model = model;
     }
-    sc.close();
+    public void getInputs(){
+        try {
+            System.out.print("Номер комнаты: ");
+            int roomNumber = sc.nextInt();
+            System.out.print("Дата заезда (dd/MM/yyyy): ");
+            Date checkIn = null;
+            try {
+                checkIn = sdf.parse(sc.next());
+            } catch (ParseException ex) {
+                ex.printStackTrace();
+            }
+            System.out.print("Дата выезда (dd/MM/yyyy): ");
+            Date checkOut = null;
+            try {
+                checkOut = sdf.parse(sc.next());
+            } catch (ParseException ex) {
+                ex.printStackTrace();
+            }
+
+            Reservation reservation = new Reservation(roomNumber, checkIn, checkOut);
+            System.out.println("Бронирование: " + reservation);
+
+            System.out.println("\nВведите данные для обновления бронирования:");
+            System.out.print("Дата заезда (dd/MM/yyyy): ");
+            checkIn = sdf.parse(sc.next());
+            System.out.print("Дата выезда (dd/MM/yyyy): ");
+            checkOut = sdf.parse(sc.next());
+
+            reservation.updateDates(checkIn, checkOut);
+            System.out.println("Бронирование: " + reservation);
+        } catch (ParseException e) {
+            System.out.println("Неверный формат даты");
+        } catch (DomainException e) {
+            System.out.println("Ошибка при бронировании: " + e.getMessage());
+        } catch (RuntimeException e) {
+            System.out.println("Неожиданная ошибка");
+        }
+        sc.close();
+
+    }
+    // Вывод данных
+    public void getOutput(String output) {
+        System.out.println(output);
+    }
 }
 
