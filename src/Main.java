f341656670a8b0f4684069cfa008ba5f5e28454d
JekBodr <jekbodr@gmail.com>
2021-12-05 22:00:33 +0200

    Rebase Main

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
-package applcations;
-
 import exceptions.WrongNightsQuantity;
 import controllers.GuestController;
 import models.Guest;
 import views.GuestView;
 
-public class GuestApp {
+public class Main {
     public static void main(String[] args) throws WrongNightsQuantity {
             Guest model = new Guest();
             GuestView view = new GuestView(model);
