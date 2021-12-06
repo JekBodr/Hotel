6c370fff7d04c8eed1207da27fdfb4745d73ce7e
JekBodr <jekbodr@gmail.com>
2021-12-07 00:29:00 +0200

    UPD name exception

--- src/utils/Validator.java
+++ src/utils/Validator.java
@@ -5,8 +5,9 @@ import exceptions.WrongDataInput;
 import java.util.Scanner;
 
 public class Validator {
-    // Валидация ввода имени
 
+    // Валидация ввода имени
+    // доработал с исключением
     public static String validateNameSymbols(Scanner scanner) throws WrongDataInput {
         String str = scanner.nextLine();
         if (str.matches("-?\\d+") ||str.isEmpty()) {
@@ -14,8 +15,6 @@ public class Validator {
         }
         return str;
     }
-
-
     // Валидация ввода количества
     public static int validateQuantityInput(Scanner scanner) {
         while (!scanner.hasNextInt()) {
@@ -34,25 +33,5 @@ public class Validator {
             quantity = scanner.nextInt();
         }
         return quantity;
-    }
-
-    // Валидация ввода цены
-    public static double validatePriceInput(Scanner scanner) {
-        while (!scanner.hasNextDouble()) {
-            String str = scanner.nextLine().trim();
-            System.out.printf("\"%s\" - не число!\n", str);
-            System.out.print("Введите цену!: ");
-        }
-        double price = scanner.nextDouble();
-        while (price <= 0) {
-            System.out.print("Неверное значение! Введите цену: ");
-            while (!scanner.hasNextDouble()) {
-                String str = scanner.nextLine().trim();
-                System.out.printf("\"%s\" - не число!\n", str);
-                System.out.print("Введите цену!: ");
-            }
-            price = scanner.nextDouble();
-        }
-        return price;
     }
 }
